3a84ad1d09803cb8931ad860254ac80f682b40ec
Agata P <43980942+agataporwit@users.noreply.github.com>
2024-01-16 16:15:38 -0800

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Agata Porwit on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
