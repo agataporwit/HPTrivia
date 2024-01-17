7fb6d308acd03450e56f0dc0369afe715777a825
Agata P <43980942+agataporwit@users.noreply.github.com>
2024-01-16 17:15:50 -0800

    Added resources and started building ContentView

---
+++ HPTrivia/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HPTrivia
+//
+//  Created by Agata Porwit on 1/16/24.
+//
+
+import Foundation
+
+enum Constants {
+    static let hpFont = "PartyLetPlain"
+}
--- HPTrivia/ContentView.swift
+++ HPTrivia/ContentView.swift
@@ -9,16 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            ZStack {
+                Image("hogwarts")
+                    .resizable()
+                    .frame(width: geo.size.width * 3, height: geo.size.height)
+                    .padding(.top, 3)
+                    .opacity(0.7)
+                
+                VStack {
+                    Image(systemName: "bolt.fill")
+                        .font(.largeTitle)
+                        .imageScale(.large)
+                    
+                    Text("HP")
+                        .font(.custom(Constants.hpFont, size: 80))
+                        .padding(.bottom, -50)
+                    
+                    Text("Trivia")
+                        .font(.custom(Constants.hpFont, size: 60))
+                }
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
         }
-        .padding()
+        .ignoresSafeArea()
     }
 }
 
-#Preview {
-    ContentView()
+
+struct ContentView_Priviews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
